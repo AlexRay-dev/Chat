e02d161d9918f0d02b49e8decaa0fedcab2bacdd
Alex <shadrinas159@gmail.com>
2022-04-30 14:17:12 +0500

    try to add authorization

--- dist/index.fbb3188c.js
+++ dist/index.fbb3188c.js
@@ -536,6 +536,8 @@ parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "UI_ELEMENTS", ()=>UI_ELEMENTS
 );
 var _dateFns = require("date-fns");
+var _jsCookie = require("js-cookie");
+var _jsCookieDefault = parcelHelpers.interopDefault(_jsCookie);
 const UI_ELEMENTS = {
     DIALOG_INPUT: document.querySelector(".dialog__message-input"),
     DIALOG_SUBMIT: document.querySelector(".dialog__message-submit"),
@@ -544,32 +546,52 @@ const UI_ELEMENTS = {
     SETTINGS_OPEN: document.querySelector(".dialog__btn-settings"),
     SETTINGS: document.querySelector("#settings"),
     AUTHORIZATION: document.querySelector("#authorization"),
-    AUTHORIZATION_SUBMIT: document.querySelector("#authorization .popup__submit"),
-    AUTHORIZATION_INPUT: document.querySelector("#authorization_input"),
+    AUTHORIZATION_MAIL_SUBMIT: document.querySelector("#authorization_mail"),
+    AUTHORIZATION_MAIL_INPUT: document.querySelector("#authorization_mail_input"),
+    AUTHORIZATION_CODE_SUBMIT: document.querySelector("#authorization_code"),
+    AUTHORIZATION_CODE_INPUT: document.querySelector("#authorization_code_input"),
     POPUP_CLOSE: document.querySelectorAll(".popup__header-close"),
     POPUP_BODY: document.querySelectorAll(".popup")
 };
+const URL = "https://mighty-cove-31255.herokuapp.com/api/user";
 UI_ELEMENTS.DIALOG_SUBMIT.addEventListener("click", function() {
     const messageText = UI_ELEMENTS.DIALOG_INPUT.value;
     UI_ELEMENTS.DIALOG_INPUT.value = "";
     UI_ELEMENTS.DIALOG_INPUT.focus();
     if (messageText) UI_ELEMENTS.MESSAGE_LIST.append(renderMessage(messageText));
 });
-UI_ELEMENTS.AUTHORIZATION_SUBMIT.addEventListener("click", function() {
-    const userMail = UI_ELEMENTS.AUTHORIZATION_INPUT.value;
-    if (userMail) {
-        const email = {
-            email: userMail
-        };
-        const json = email ? JSON.stringify(email) : false;
-        console.log(loadMail(json));
-    // if(loadMail(json).ok) {
-    //     console.log(true);
-    // }
-    } else alert('введите корректную почту');
-});
+getCode();
+enterCode();
+function getCode() {
+    UI_ELEMENTS.AUTHORIZATION_MAIL_SUBMIT.addEventListener("click", function() {
+        const userMail = UI_ELEMENTS.AUTHORIZATION_MAIL_INPUT.value;
+        if (userMail) {
+            const email = {
+                email: userMail
+            };
+            const json = email ? JSON.stringify(email) : false;
+        } else alert("введите корректную почту");
+    });
+}
+function enterCode() {
+    UI_ELEMENTS.AUTHORIZATION_CODE_SUBMIT.addEventListener("click", function() {
+        const userCode = UI_ELEMENTS.AUTHORIZATION_CODE_INPUT.value;
+        if (userCode) {
+            _jsCookieDefault.default.set("token", userCode);
+            const token = _jsCookieDefault.default.get("token");
+            fetch(URL, {
+                method: "PATCH",
+                headers: {
+                    Authorization: `bearer ${token}`
+                },
+                body: {
+                    name: "Alex"
+                }
+            });
+        }
+    });
+}
 async function loadMail(email) {
-    const URL = "https://mighty-cove-31255.herokuapp.com/api/user";
     const response = await fetch(URL, {
         method: "POST",
         headers: {
@@ -590,7 +612,7 @@ function renderMessage(text) {
     return message;
 }
 
-},{"date-fns":"9yHCA","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"9yHCA":[function(require,module,exports) {
+},{"date-fns":"9yHCA","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","js-cookie":"c8bBu"}],"9yHCA":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 // This file is generated automatically by `scripts/build/indices.js`. Please, don't change it.
@@ -3635,7 +3657,97 @@ var quartersInYear = 4;
 var secondsInHour = 3600;
 var secondsInMinute = 60;
 
-},{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"fkh2R":[function(require,module,exports) {
+},{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"c8bBu":[function(require,module,exports) {
+(function(global, factory) {
+    module.exports = factory();
+})(this, function() {
+    'use strict';
+    /* eslint-disable no-var */ function assign(target) {
+        for(var i = 1; i < arguments.length; i++){
+            var source = arguments[i];
+            for(var key in source)target[key] = source[key];
+        }
+        return target;
+    }
+    /* eslint-enable no-var */ /* eslint-disable no-var */ var defaultConverter = {
+        read: function(value) {
+            if (value[0] === '"') value = value.slice(1, -1);
+            return value.replace(/(%[\dA-F]{2})+/gi, decodeURIComponent);
+        },
+        write: function(value) {
+            return encodeURIComponent(value).replace(/%(2[346BF]|3[AC-F]|40|5[BDE]|60|7[BCD])/g, decodeURIComponent);
+        }
+    };
+    /* eslint-enable no-var */ /* eslint-disable no-var */ function init(converter1, defaultAttributes) {
+        function set(key, value, attributes) {
+            if (typeof document === 'undefined') return;
+            attributes = assign({}, defaultAttributes, attributes);
+            if (typeof attributes.expires === 'number') attributes.expires = new Date(Date.now() + attributes.expires * 864e5);
+            if (attributes.expires) attributes.expires = attributes.expires.toUTCString();
+            key = encodeURIComponent(key).replace(/%(2[346B]|5E|60|7C)/g, decodeURIComponent).replace(/[()]/g, escape);
+            var stringifiedAttributes = '';
+            for(var attributeName in attributes){
+                if (!attributes[attributeName]) continue;
+                stringifiedAttributes += '; ' + attributeName;
+                if (attributes[attributeName] === true) continue;
+                // Considers RFC 6265 section 5.2:
+                // ...
+                // 3.  If the remaining unparsed-attributes contains a %x3B (";")
+                //     character:
+                // Consume the characters of the unparsed-attributes up to,
+                // not including, the first %x3B (";") character.
+                // ...
+                stringifiedAttributes += '=' + attributes[attributeName].split(';')[0];
+            }
+            return document.cookie = key + '=' + converter1.write(value, key) + stringifiedAttributes;
+        }
+        function get(key) {
+            if (typeof document === 'undefined' || arguments.length && !key) return;
+            // To prevent the for loop in the first place assign an empty array
+            // in case there are no cookies at all.
+            var cookies = document.cookie ? document.cookie.split('; ') : [];
+            var jar = {};
+            for(var i = 0; i < cookies.length; i++){
+                var parts = cookies[i].split('=');
+                var value = parts.slice(1).join('=');
+                try {
+                    var foundKey = decodeURIComponent(parts[0]);
+                    jar[foundKey] = converter1.read(value, foundKey);
+                    if (key === foundKey) break;
+                } catch (e) {}
+            }
+            return key ? jar[key] : jar;
+        }
+        return Object.create({
+            set: set,
+            get: get,
+            remove: function(key, attributes) {
+                set(key, '', assign({}, attributes, {
+                    expires: -1
+                }));
+            },
+            withAttributes: function(attributes) {
+                return init(this.converter, assign({}, this.attributes, attributes));
+            },
+            withConverter: function(converter) {
+                return init(assign({}, this.converter, converter), this.attributes);
+            }
+        }, {
+            attributes: {
+                value: Object.freeze(defaultAttributes)
+            },
+            converter: {
+                value: Object.freeze(converter1)
+            }
+        });
+    }
+    var api = init(defaultConverter, {
+        path: '/'
+    });
+    /* eslint-enable no-var */ return api;
+});
+
+},{}],"fkh2R":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "popupActivated", ()=>popupActivated
--- js/view.js
+++ js/view.js
@@ -1,4 +1,5 @@
 import { format } from "date-fns";
+import Cookies from "js-cookie";
 
 export const UI_ELEMENTS = {
   DIALOG_INPUT: document.querySelector(".dialog__message-input"),
@@ -9,12 +10,16 @@ export const UI_ELEMENTS = {
   SETTINGS_OPEN: document.querySelector(".dialog__btn-settings"),
   SETTINGS: document.querySelector("#settings"),
   AUTHORIZATION: document.querySelector("#authorization"),
-  AUTHORIZATION_SUBMIT: document.querySelector("#authorization .popup__submit"),
-  AUTHORIZATION_INPUT: document.querySelector("#authorization_input"),
+  AUTHORIZATION_MAIL_SUBMIT: document.querySelector("#authorization_mail"),
+  AUTHORIZATION_MAIL_INPUT: document.querySelector("#authorization_mail_input"),
+  AUTHORIZATION_CODE_SUBMIT: document.querySelector("#authorization_code"),
+  AUTHORIZATION_CODE_INPUT: document.querySelector("#authorization_code_input"),
 
   POPUP_CLOSE: document.querySelectorAll(".popup__header-close"),
   POPUP_BODY: document.querySelectorAll(".popup"),
 };
+
+const URL = "https://mighty-cove-31255.herokuapp.com/api/user";
 
 UI_ELEMENTS.DIALOG_SUBMIT.addEventListener("click", function () {
   const messageText = UI_ELEMENTS.DIALOG_INPUT.value;
@@ -24,25 +29,40 @@ UI_ELEMENTS.DIALOG_SUBMIT.addEventListener("click", function () {
     UI_ELEMENTS.MESSAGE_LIST.append(renderMessage(messageText));
   }
 });
+getCode();
+enterCode();
 
-UI_ELEMENTS.AUTHORIZATION_SUBMIT.addEventListener("click", function () {
-  const userMail = UI_ELEMENTS.AUTHORIZATION_INPUT.value;
-  if (userMail) {
-    const email = {
-      email: userMail,
-    };
-    const json = email ? JSON.stringify(email) : false;
-    console.log(loadMail(json));
-    // if(loadMail(json).ok) {
-    //     console.log(true);
-    // }
-  } else {
-      alert('введите корректную почту')
-  }
-});
+function getCode() {
+  UI_ELEMENTS.AUTHORIZATION_MAIL_SUBMIT.addEventListener("click", function () {
+    const userMail = UI_ELEMENTS.AUTHORIZATION_MAIL_INPUT.value;
+    if (userMail) {
+      const email = {
+        email: userMail,
+      };
+      const json = email ? JSON.stringify(email) : false;
+    } else {
+      alert("введите корректную почту");
+    }
+  });
+}
+function enterCode() {
+  UI_ELEMENTS.AUTHORIZATION_CODE_SUBMIT.addEventListener("click", function () {
+    const userCode = UI_ELEMENTS.AUTHORIZATION_CODE_INPUT.value;
+    if (userCode) {
+      Cookies.set("token", userCode);
+      const token = Cookies.get("token");
+      fetch(URL, {
+        method: "PATCH",
+        headers: {
+            Authorization: `bearer ${token}`,
+        },
+        body: { name: "Alex" },
+      });
+    }
+  });
+}
 
 async function loadMail(email) {
-  const URL = "https://mighty-cove-31255.herokuapp.com/api/user";
   const response = await fetch(URL, {
     method: "POST",
     headers: {
